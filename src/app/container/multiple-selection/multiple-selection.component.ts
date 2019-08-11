0a01251dda70970006ec5f86877d06af121224b5
RosDiaf <ros_diaf@hotmail.com>
2019-08-11 18:16:50 +0100

    Multiple selection with checkboxes added

--- src/app/container/multiple-selection/multiple-selection.component.ts
+++ src/app/container/multiple-selection/multiple-selection.component.ts
@@ -10,23 +10,36 @@ export class MultipleSelectionComponent implements OnInit {
   @Input() dataset: any;
   @Output() onMultipleSelection: EventEmitter<any> = new EventEmitter();
   groupNames: any;
-  selectedItems: any;
+  // selectedItems: any;
   isToggleHeader: boolean;
+  groupSelected: any = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmit() {
-    this.onMultipleSelection.emit(this.selectedItems);
-  }
+  // onSubmit() {
+  //   this.onMultipleSelection.emit(this.selectedItems);
+  // }
 
-  resetSlection() {
-    this.selectedItems.length = 0;
+  resetSlection(event) {
+    console.log(event)
+    // this.selectedItems.length = 0;
+    this.groupSelected.length = 0;
+    this.onMultipleSelection.emit(this.groupSelected);
   }
 
   toggleHeader() {
     this.isToggleHeader = !this.isToggleHeader;
+  }
+
+  onChangeCategory(event, item: any) {
+    if(event.target.checked && this.groupSelected.indexOf(item) === -1) {
+      this.groupSelected.push(item)
+    } else if(!event.target.checked) {
+      this.groupSelected.pop(item)
+    }
+    this.onMultipleSelection.emit(this.groupSelected);
   }
 }
